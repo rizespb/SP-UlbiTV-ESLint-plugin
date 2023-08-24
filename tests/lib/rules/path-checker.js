861f60ddf50e7abb084797427d1f5fbd72e0e990
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2023-08-24 10:22:53 +0300

    89 Опции в линтере. Добавляем поддержку алисов в наш eslint плагин

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -1,31 +1,54 @@
-/**
- * @fileoverview feature sliced relative path checker
- * @author rizespb
- */
-"use strict";
+// Тесты для правила path-checker
+
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/path-checker"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/path-checker'),
+  RuleTester = require('eslint').RuleTester
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("path-checker", rule, {
+const ruleTester = new RuleTester({
+  // Для паринга EcmaScript с import/export надо передать настройки
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+})
+ruleTester.run('path-checker', rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      // Это пример адреса файла, в котором мы представляем, что есть код, указанный в свойстве code
+      filename: 'C:\\Users\\AMaklachkov\\Desktop\\SP-UlbiTV-Project\\src\\entities\\Article',
+      // Это пример импортов в указанном файле
+      // Тут всё корректно: внутри слайса используются относительные имопрты
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "gfhdfg",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      // Ошибка, которую должен показать линтер
+      errors: [{ message: 'В рамках одного слайса все пути должны быть относительными' }],
+      // Передаем алиас в правило
+      options: [
+        {
+          alias: '@',
+        },
+      ],
+    },
+    {
+      // Это пример адреса файла, в котором мы представляем, что есть код, указанный в свойстве code
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      // Это пример импортов в указанном файле
+      // Ошибка в том, что в Article, который лежит в entities, используется абсолютный импорт из entities
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: 'В рамках одного слайса все пути должны быть относительными' }],
     },
   ],
-});
+})
